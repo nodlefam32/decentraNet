78949afe89fe8084f9338262c23c3444740d5356
LazySloth456 <eldon9163@gmail.com>
2022-06-07 13:05:15 +1200

    Trades RSA 2048 bit keys and anonymous mode added

--- main/DecentraNet.py
+++ main/DecentraNet.py
@@ -3,6 +3,8 @@ import threading
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto import Random
+from Crypto.Random import get_random_bytes
+from base64 import b64encode
 import socket
 import random
 import time
@@ -10,18 +12,21 @@ import os
 
 
 class decentraNet:
-    def __init__(self, IP, PORT, networkingDevice):
-        self.IP = IP
+    def __init__(self, anonymousMode, PORT, networkingDevice):
         self.PORT = PORT
         self.pubKey = b''
         self.privKey = b''
         self.publicKeys = []
+        self.sharedSecrets = []
+        self.anonymousMode = anonymousMode
         self.ADDR = ('192.168.1.255', PORT)
+        self.mode = 1  # mode 1 = sending data, mode 2 = receiving data
         self.networkingDevice = networkingDevice
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.listen = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def generateKeys(self):
+
         # General introduction
         print("---Welcome to the decentralized IoT---")
         print("This builds emergency IoT infrastructure")
@@ -34,8 +39,8 @@ class decentraNet:
         print("Checking for existence of private and public keys these are required to browse")
         print("To get a new identity delete the private and public key files.")
 
-        # Generate new key pair if one file does not exist
-        if os.path.exists("private.key") and os.path.exists("public.key"):
+        # Read old pair if one does exist
+        if os.path.exists("private.key") and os.path.exists("public.key") and self.anonymousMode is False:
             with open("private.key", "rb") as f:
                 self.privKey = f.read()
                 f.close()
@@ -44,10 +49,11 @@ class decentraNet:
                 f.close()
             print("Using old key delete private.key or public.key and run this program again to generate a new pair")
             print(f"{self.privKey}\n{self.pubKey}")
-        else:
-            keyPair = RSA.generate(1024, Random.new().read)  # Generate 2048-bit RSA keys must be broken for each user
+        else: # Generate new pair if one doesn't exist
+            keyPair = RSA.generate(2048, Random.new().read)  # Generate unique 2048-bit RSA
             self.pubKey = keyPair.public_key().exportKey()
             self.privKey = keyPair.exportKey()
+
             with open("private.key", "wb") as f:
                 f.write(self.privKey)
                 f.close()
@@ -60,12 +66,14 @@ class decentraNet:
             print(f"{self.privKey}\n{self.pubKey}")
 
     def configureNetworkDevice(self, networkingDevice):
+        # Ad-hoc is only required for Wi-Fi
         os.system(f"sudo iwconfig {networkingDevice} mode Ad-hoc")
         print("Set networking mode to Ad-hoc")
+        # essid is only required for Wi-Fi
         os.system(f"sudo iwconfig {networkingDevice} essid TDN")
         print("Set networking essid to TDN")
-        os.system(f"sudo ifconfig {networkingDevice} 192.168.1.1 netmask 255.255.255.0")
-        print("Set IP to 192.168.1.1 and netmask to 255.255.255.0")
+        os.system(f"sudo ifconfig {networkingDevice} 192.168.1.{self.mode} netmask 255.255.255.0")
+        print(f"Set IP to 192.168.1.{self.mode} and netmask to 255.255.255.0")
         print("Finished device configuration")
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.listen.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -79,64 +87,63 @@ class decentraNet:
             self.s.sendto(self.pubKey, self.ADDR)
             print("Client Sent: ", self.pubKey)
 
-        """
-        print("Waiting for response")
-
-        startTime = time.time()
-        while time.time() < startTime + 2.5:  # Aggressively receive responses for 2.5 seconds
-            self.listen.setblocking(0)  # Keep letting those messages in don't wait for receives
-            try:
-                data, address = self.listen.recvfrom(16384)  # Accept responses; do not wait
-                print(data, "\nAbove is my stupid response I received")
-            except:
-                pass
-        self.listen.setblocking(1)  # Go back to waiting you sussy baka
-        """
-
     def listenForDevices(self):
-        os.system(f"sudo ifconfig {networkingDevice} 192.168.1.3 netmask 255.255.255.0")
         print("Listening for devices")
         while True:
+            self.mode = 2
+            os.system(f"sudo ifconfig {networkingDevice} 192.168.1.{self.mode} netmask 255.255.255.0")
             key, address = self.listen.recvfrom(16384)
-            if not self.publicKeys.__contains__(key) and len(key) == 271 and key != self.pubKey:
+            if not self.publicKeys.__contains__(key) and not key.__contains__(self.pubKey) and len(key) == 450:
+                # Process new raw public key
                 try:
+                    # Verify the key received is real
                     publicKey = RSA.importKey(key)
                     publicKey = PKCS1_OAEP.new(publicKey)
-                    lettersAvailable = string.ascii_letters + string.digits
-                    sharedSecret = bytes(str(''.join(random.choice(lettersAvailable) for x in range(86))), 'utf-8')
-                    print('your text before encryption is: {}'.format(sharedSecret))
-                    encrypted_text = publicKey.encrypt(sharedSecret)
-                    # rsa_private_key = RSA.importKey(self.privKey)
-                    # rsa_private_key = PKCS1_OAEP.new(rsa_private_key)
-                    # decrypted_text = rsa_private_key.decrypt(encrypted_text)
-                    # print(decrypted_text)
+                    sharedSecret = get_random_bytes(16 * 2)
+                    encryptedSharedKey = publicKey.encrypt(sharedSecret)
 
-                    # print('your decrypted_text is: {}'.format(decrypted_text))
-                    print(encrypted_text, len(encrypted_text))
+                    print(encryptedSharedKey, len(encryptedSharedKey))
                     self.publicKeys.append(key)
+                    self.sharedSecrets.append(sharedSecret)
                     if len(self.publicKeys) > 16384:
                         self.publicKeys.remove(0)
+                        self.sharedSecrets.remove(0)
                     print("received new key:", key)
-                    os.system(f"sudo ifconfig {networkingDevice} 192.168.1.2 netmask 255.255.255.0")
+                    self.mode = 1
+                    os.system(f"sudo ifconfig {networkingDevice} 192.168.1.{self.mode} netmask 255.255.255.0")
                     for trialError in range(3):  # We loop 3 times to increase chances of discovering devices successfully
                         # Create socket for server
                         # Let's send data through UDP protocol
-                        self.s.sendto(self.pubKey, self.ADDR)
-                        print("Client Sent:", self.pubKey)
+                        self.s.sendto(self.pubKey + encryptedSharedKey, self.ADDR)  # Your public key and a shared key
+                        print("Client Sent:", self.pubKey + encryptedSharedKey)
                 except:
-                    print("Invalid key received ignoring")
+                    print("Invalid key received")
+            elif not self.publicKeys.__contains__(key[0:450]) and not key.__contains__(self.pubKey) and len(key) > 450:
+                try:
+                    print("New key to process:", key)
+                    IP = key[0:450]
+                    secret = key[450:]
+                    rsa_private_key = RSA.importKey(self.privKey)
+                    rsa_private_key = PKCS1_OAEP.new(rsa_private_key)
+                    decryptedSharedKey = rsa_private_key.decrypt(secret)
+                    self.publicKeys.append(IP)
+                    self.sharedSecrets.append(decryptedSharedKey)
+                    if len(self.publicKeys) > 16384:
+                        self.publicKeys.remove(0)
+                        self.sharedSecrets.remove(0)
+                except:
+                    print("Invalid key received")
 
 
 if __name__ == '__main__':
-    IP = '192.168.1.1'
     PORT = 1050
+    anonymousMode = True
     networkingDevice = 'eno1'
-    while True:
-        net = decentraNet(IP, PORT, networkingDevice)
-        net.configureNetworkDevice(networkingDevice)
-        net.generateKeys()
-        net.searchForDevices()
-        net.listenForDevices()
+    net = decentraNet(anonymousMode, PORT, networkingDevice)
+    net.configureNetworkDevice(networkingDevice)
+    net.generateKeys()
+    net.searchForDevices()
+    net.listenForDevices()
 else:
     exit("You cannot import DecentraNet.")
 
@@ -147,3 +154,4 @@ We then configure the network card
 We then scan the network for live devices
 Bam we can use the network and access its services but remember to trade keys lol
 """
+
